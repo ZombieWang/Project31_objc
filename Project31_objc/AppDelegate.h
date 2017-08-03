239211b474963c5ea369d61bd05d7295da15e5f5
Frank <frank7300228@gmail.com>
2017-08-04 01:26:58 +0800

    Initial Commit

---
+++ Project31_objc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Project31_objc
+//
+//  Created by Frank on 2017/8/4.
+//  Copyright © 2017年 Frank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
